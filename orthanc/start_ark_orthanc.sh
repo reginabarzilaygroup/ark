307daf5bc8e900926924af8f39758f8819d672fd
Jacob Silterra <silterra@mit.edu>
2024-10-09 10:29:08 -0400

    Enable setting Orthanc Dicom AE title and port at runtime via environment variable

--- orthanc/start_ark_orthanc.sh
+++ orthanc/start_ark_orthanc.sh
@@ -2,8 +2,14 @@
 
 model_name=${1:-"mirai"}
 
+# Replace value in config file with environment variable, if present.
+# Can't use Orthanc built-in mechanism because it needs to be an integer.
+ARK_DICOM_PORT=${ARK_DICOM_PORT:-4242}
+sed "s/\$ARK_DICOM_PORT/${ARK_DICOM_PORT}/" orthanc/orthanc.json > orthanc/tmp_orthanc.json
+cat orthanc/tmp_orthanc.json
+
 # Launch Orthanc
-Orthanc ./orthanc/orthanc.json &
+Orthanc ./orthanc/tmp_orthanc.json &
 sleep 5
 
 # Run regular ark
